fe4e99f8b5e92f575e93aee5a9d821cf3a8ed0a2
Julie Kristoffersen <julkri02243@stud.noroff.no>
2025-05-06 21:39:25 +0200

    First commit for login page
    Completed, but missing images. I tried to divide up the CSS to make it easier to read.

---
+++ src/login-check.js
@@ -0,0 +1,56 @@
+import { mobileMenu } from './login-mobile-menu.mjs';
+mobileMenu();
+
+const form = document.querySelector('#login-form');
+
+form.addEventListener('submit', (e) => {
+  let email = document.querySelector('#email');
+  let password = document.querySelector('#password');
+
+  if (
+    email.value.includes('@') &&
+    email.value.includes('.') &&
+    password.value !== ''
+  ) {
+    e.preventDefault();
+    window.location.href = '../dashboard/student/index.html';
+  } else {
+    e.preventDefault();
+    //displays alert
+    document.querySelector('.error-container').style.display = 'block';
+
+    //checks for missing inputs
+    if (email.value === '') {
+      document.querySelector('.email').style.borderColor = '#FF5722';
+      document.querySelector('.email-error').style.display = 'block';
+      document.querySelector('#email-error').innerText = 'Please input email.';
+    } else {
+      if (email.value.includes('@') && email.value.includes('.')) {
+        document.querySelector('.email').style.borderColor = '#CCCCCC';
+        document.querySelector('.email-error').style.display = 'none';
+      } else {
+        document.querySelector('.email').style.borderColor = '#FF5722';
+        document.querySelector('.email-error').style.display = 'block';
+        document.querySelector('#email-error').innerText =
+          'Please input valid email.';
+      }
+    }
+
+    if (password.value === '') {
+      document.querySelector('.password').style.borderColor = '#FF5722';
+      document.querySelector('.password-error').style.display = 'block';
+      document.querySelector('#password-error').innerText =
+        'Please input password.';
+    } else {
+      document.querySelector('.password').style.borderColor = '#CCCCCC';
+      document.querySelector('.password-error').style.display = 'none';
+    }
+  }
+
+  //close alert
+  document
+    .querySelector('#x-button')
+    .addEventListener('click', function removeAlert() {
+      document.querySelector('.error-container').style.display = 'none';
+    });
+});
