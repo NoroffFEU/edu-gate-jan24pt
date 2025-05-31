9ae008f76386d38e3a8743ad99bf5ce93a8985bf
siljeloge <silje_loge@hotmail.com>
2025-05-31 20:10:19 +0200

    javascript changes

--- src/landingpage-mobile.js
+++ src/landingpage-mobile.js
@@ -2,11 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const hamburgerIcon = document.getElementById("hamburgericon");
     const burgerLinks = document.querySelector(".burgerlinks");
   
-    burgerLinks.style.display = "none"; // Optional: if not hidden by default in CSS
+    burgerLinks.style.display = "none";
   
     hamburgerIcon.addEventListener("click", function () {
       console.log("Hamburger clicked");
       const isVisible = burgerLinks.style.display === "flex";
       burgerLinks.style.display = isVisible ? "none" : "flex";
+    });
+
+    window.addEventListener("resize", function () {
+      if (window.innerWidth > 768) {
+        burgerLinks.style.display = "none";
+      } 
     });
   });
