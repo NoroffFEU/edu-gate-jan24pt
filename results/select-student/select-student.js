8b36814a7257adf77641ec0702203bb2399405c5
theresesvaeren <thesva02455@stud.noroff.no>
2025-08-29 23:39:10 +0200

    Implement the Result Table, HTML, CSS and JS.

---
+++ results/select-student/select-student.js
@@ -0,0 +1,27 @@
+function showSidebar(){
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "flex"
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "none"
+}
+
+
+
+  function toggleLeftSidebar() {
+    const leftSidebar = document.querySelector('.left-sidebar');
+    leftSidebar.classList.toggle('active');
+  }
+
+
+  function showLeftSidebar(){
+    const sidebar = document.querySelector(".left-sidebar")
+    sidebar.style.display = "flex"
+}
+
+function hideLeftSidebar(){
+    const sidebar = document.querySelector(".left-sidebar")
+    sidebar.style.display = "none"
+}
